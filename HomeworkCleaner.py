023958370cd52205c35007eb23a949de5a32008d
bssthu <bssda03@gmail.com>
2014-12-26 15:02:46 +0800

    add py file

---
+++ HomeworkCleaner.py
@@ -0,0 +1,17 @@
+#! D:\Python33\python.exe
+# -*- coding: utf-8 -*-
+# Module        : HomeworkCleaner.py
+# Author        : bss
+# Project       : TA
+# State         :
+# Creation Date : 2014-12-26
+#  Last modified: 2014-12-26 14:56:50
+# Description   : 
+# 
+
+import os
+
+if __name__ == '__main__':
+    path = os.path.abspath('.')
+    print(path)
+    os.system('pause')

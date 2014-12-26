a616a6c58c39f8546ac6478e3f91ccfa27567ac6
bssthu <bssda03@gmail.com>
2014-12-26 16:01:59 +0800

    compress to zip

--- HomeworkCleaner.py
+++ HomeworkCleaner.py
@@ -5,13 +5,38 @@
 # Project       : TA
 # State         :
 # Creation Date : 2014-12-26
-#  Last modified: 2014-12-26 14:56:50
+#  Last modified: 2014-12-26 16:00:21
 # Description   : 
 # 
 
 import os
+import zipfile
+
+def cleanDir(dirname):
+    pass
+
+def zipDir(cd, dirname):
+    zipName = os.path.join(cd, dirname + '.zip')
+    f = zipfile.ZipFile(zipName, 'w', zipfile.ZIP_DEFLATED)
+    searchDir = os.path.join(cd, dirname)
+    for dirpath, dirnames, filenames in os.walk(searchDir):
+        for filename in filenames:
+            f.write(os.path.join(dirpath, filename),
+                    os.path.join(dirpath, filename)[searchDir.__len__():])
+    f.close()
+
+def main():
+    cd = os.path.abspath('.')
+    print(cd)
+    for dirname in os.listdir(cd):
+        if os.path.isdir(os.path.join(cd, dirname)) and (dirname != '.git'):
+            print(dirname + ':')
+            zipDir(cd, dirname)
 
 if __name__ == '__main__':
-    path = os.path.abspath('.')
-    print(path)
+    try:
+        main()
+    except:
+        pass
     os.system('pause')
+ 

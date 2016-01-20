008001570cad21a704aa73a64fad9d0d8357a09f
bssthu <bssthu@users.noreply.github.com>
2016-01-20 19:15:34 +0800

    follow PEP8's advice

--- HomeworkCleaner.py
+++ HomeworkCleaner.py
@@ -11,25 +11,27 @@
 import os
 import zipfile
 
-g_basicList = ['obj','tlog','pdb','ilk','idb','log','lastbuildstate',
-        'manifest','res','rc','cache','cs','resources','baml','lref',
-        'exe.config','filelistabsolute.txt','pch','opt','o','cpp','h',
-        'enc','dep','tlh','tli','sbr','bsc','dll.bi','lastcodeanalysissucceeded',
-        'nativecodeanalysis.xml','nativecodeanalysis.all.xml']
+g_basicList = [
+    'obj', 'tlog', 'pdb', 'ilk', 'idb', 'log', 'lastbuildstate',
+    'manifest', 'res', 'rc', 'cache', 'cs', 'resources', 'baml', 'lref',
+    'exe.config', 'filelistabsolute.txt', 'pch', 'opt', 'o', 'cpp', 'h',
+    'enc', 'dep', 'tlh', 'tli', 'sbr', 'bsc', 'dll.bi', 'lastcodeanalysissucceeded',
+    'nativecodeanalysis.xml', 'nativecodeanalysis.all.xml'
+]
+
 
 # 是否要删除此文件
-def checkClearDir(path, filenames, choose):
+def check_clear_dir(path, file_names, choose):
     s = os.sep
-    if (path.find('__MACOSX'+s) >= 0
-            or path.endswith('.DS_Store')):
+    if path.find('__MACOSX'+s) >= 0 or path.endswith('.DS_Store'):
         return True
-    path = path.lower() # windows
-    if (path.endswith('d.dll')):
-        dllname = path.split(os.sep)[-1][:-5]
-        for filename in filenames:
-            if dllname == filename[:-4]:
+    path = path.lower()  # windows
+    if path.endswith('d.dll'):
+        dll_name = path.split(os.sep)[-1][:-5]
+        for filename in file_names:
+            if dll_name == filename[:-4]:
                 return True
-    if (path.endswith('.dll') or path.endswith('.exe')):
+    if path.endswith('.dll') or path.endswith('.exe'):
         return False
     if (path.endswith('.sdf') or
             path.endswith('.opensdf') or
@@ -48,7 +50,7 @@ def checkClearDir(path, filenames, choose):
             path.endswith('.gz')):
         print('>archive ' + path.split(os.sep)[-1])
     if (path.find('debug'+s) >= 0 or
-            path.find('release'+s) >= 0) :
+            path.find('release'+s) >= 0):
         if 1 == choose:     # basic
             for ext in g_basicList:
                 if path.endswith('.' + ext):
@@ -58,16 +60,18 @@ def checkClearDir(path, filenames, choose):
             return True
     return False
 
-def zipDir(cd, dirname, choose):
-    zipName = os.path.join(cd, dirname + '.zip')
-    f = zipfile.ZipFile(zipName, 'w', zipfile.ZIP_DEFLATED)
-    searchDir = os.path.join(cd, dirname)
-    for dirpath, dirnames, filenames in os.walk(searchDir):
-        for filename in filenames:
-            fileAbsPath = os.path.join(dirpath, filename)
-            if not checkClearDir(fileAbsPath, filenames, choose):
-                f.write(fileAbsPath, fileAbsPath[searchDir.__len__():])
+
+def zip_dir(cd, dir_name, choose):
+    zip_name = os.path.join(cd, dir_name + '.zip')
+    f = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
+    search_dir = os.path.join(cd, dir_name)
+    for dir_path, dir_names, file_names in os.walk(search_dir):
+        for file_name in file_names:
+            file_abs_path = os.path.join(dir_path, file_name)
+            if not check_clear_dir(file_abs_path, file_names, choose):
+                f.write(file_abs_path, file_abs_path[search_dir.__len__():])
     f.close()
+
 
 def main():
     cd = os.path.abspath('.')
@@ -78,17 +82,17 @@ def main():
         choose = int(choose)
     except:
         pass
-    if (choose != 1 and choose != 2):
+    if choose != 1 and choose != 2:
         print('bye~')
         return
 
-    for dirname in os.listdir(cd):
-        if os.path.isdir(os.path.join(cd, dirname)) and (dirname != '.git'):
-            print(dirname + ':')
-            zipDir(cd, dirname, choose)
+    for dir_name in os.listdir(cd):
+        if os.path.isdir(os.path.join(cd, dir_name)) and (dir_name != '.git'):
+            print(dir_name + ':')
+            zip_dir(cd, dir_name, choose)
     print('nice!')
+
 
 if __name__ == '__main__':
     main()
     os.system('pause')
- 
